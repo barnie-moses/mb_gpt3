db5bd4fbe06628c705536efbdd150b926201e9ae
barniemo <barniemo@msu.edu>
2024-12-20 01:44:42 -0500

    complent gpt-3 App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
- import React from 'react'
- 
- const App = () => {
-   return (
-     <div>
-       <h1>GPT-3</h1>
-     </div>
-   )
- }
- 
- export default App
- 
+import React from 'react';
+import { Footer, Possibility, Blog, Features, WhatGPT3, Header} from './containers';
+import {CTA, Navbar, Brand } from './components';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
